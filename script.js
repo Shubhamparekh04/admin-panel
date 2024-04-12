51fda9b3c376dacfba1f5bcbb2471001b4dfbbb7
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-04-13 01:15:55 +0530

    navbar/popup

--- script.js
+++ script.js
@@ -1,75 +1,40 @@
-// $("#dashboard,#forms,#tables,#charts,#icons,#uielements,#authentication,#pages,#users,#settings").css("display", "none");
-// // 1
-// $("#dashboardBtn").on("click", function () {
-//   $("#forms,#tables,#charts,#icons,#uielements,#authentication,#pages,#users,#settings").css("display", "none");
-//   $("#dashboard").css("display", "block");
-// });
-// // 2
-// $("#formsBtn").on("click", function () {
-//   $("#dashboard,#tables,#charts,#icons,#uielements,#authentication,#pages,#users,#settings").css("display", "none");
-//   $("#forms").css("display", "block");
-// });
+// navbar links mouseover-leave color effect
+$("#span1,#span2").on("mouseenter", function () {
+  $(this).css("color", "white");
+});
+$("#span1,#span2").on("mouseleave", function () {
+  $(this).css("color", "#B2EBF2");
+});
+// //navbar links hover color effect
 
-// // 3
-// $("#tablesBtn").on("click", function () {
-//   $("#dashboard,#forms,#charts,#icons,#uielements,#authentication,#pages,#users,#settings").css("display", "none");
-//   $("#tables").css("display", "block");
-// });
 
-// // 4
-// $("#chartsBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#icons,#uielements,#authentication,#pages,#users,#settings").css("display", "none");
-//     $("#charts").css("display", "block");
-//   });
-// // 5
-// $("#iconsBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#charts,#uielements,#authentication,#pages,#users,#settings").css("display", "none");
-//     $("#icons").css("display", "block");
-//   });
-// // 6
-// $("#uiBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#charts,#icons,#authentication,#pages,#users,#settings").css("display", "none");
-//     $("#uielements").css("display", "block");
-//   });
-// // 7
-// $("#authBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#charts,#icons,#uielements,#pages,#users,#settings").css("display", "none");
-//     $("#authentication").css("display", "block");
-//   });
-// // 8
-// $("#pagesBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#charts,#icons,#uielements,#authentication,#users,#settings").css("display", "none");
-//     $("#pages").css("display", "block");
-//   });
-// // 9
-// $("#usersBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#charts,#icons,#uielements,#authentication,#pages,#settings").css("display", "none");
-//     $("#users").css("display", "block");
-//   });
-// // 10
-// $("#settingsBtn").on("click", function () {
-//     $("#dashboard,#forms,#tables,#charts,#icons,#uielements,#authentication,#pages,#users").css("display", "none");
-//     $("#settings").css("display", "block");
-//   });
-
+// sidebar button click which one to show
 $("#content>div").hide();
 $("#dashboard").show();
-
 $("#sidebar button").on("click", function () {
-  $("#content>div").hide();
-  $("#" + $(this).attr("for")).show();
-});
+  $("#content>div, .model1 , .modelbg").hide();
+  $($(this).attr("for")).show();
 
-var hidden = false;
-
-$("#menu").on("click", function () {
-  if (hidden === false) {
-    $("#sidebar").fadeOut();
-    $("#content").css("width", "100%");
-    hidden = true;
-  } else {
-    $("#sidebar").fadeIn();
-    $("#content").css("width", "80%");
-    hidden=false;
+  if($(this).attr("for")==="#tables"){
+    $("#tables>img").attr("src","https://picsum.photos/999/512");
+    $($(this).attr("for")).show();
   }
 });
+// //sidebar button click which one to show
+
+// sidebar hide show
+$("#menu").on("click", function () {
+  $("#sidebar").toggle("linear", function () {
+    $("#content").css("width", "100%");
+    $(".model1 , .modelbg").hide();
+  });
+});
+// //sidebar hide show
+
+
+// popup
+$(".model1,.modelbg").hide();
+$(".fa-spider").on("click", function () {
+  $(".model1,.modelbg").toggle();
+});
+// //popup

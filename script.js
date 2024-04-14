b96e5ec106a3def9d5d78baa912738576ded9807
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-04-15 00:00:18 +0530

    chart js integration

--- script.js
+++ script.js
@@ -38,3 +38,35 @@ $(".fa-spider").on("click", function () {
   $(".model1,.modelbg").toggle();
 });
 // //popup
+
+
+
+// //chart
+(async function() {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+  
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'radar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
+// //chart
